bd60d99a8a8adb6b68b7b84ba5239254a3eb19b2
adi1234-art <72471729+adi1234-art@users.noreply.github.com>
2022-06-07 11:15:55 +0200

    zad10

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
 
 namespace WebApplication1
 {
@@ -24,6 +26,9 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
